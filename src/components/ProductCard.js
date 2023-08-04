3e5c7a918460972c3a10aee0f0f3c2edae892746
Keely001 <thindiuke@gmail.com>
2023-08-04 18:52:02 +0300

    added functionality

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,7 +1,23 @@
 import "./ProductCard.css"
+import {useCart} from "../context/CartContext"
+// import { useState } from "react";
+// import { useEffect } from "react";
 
-export const ProductCard = ({product, handleClick}) => {
-    const { name, price, image} = product;
+export const ProductCard = ({product}) => {
+    const {  name, price, image} = product;
+    const {addToCart} = useCart();
+    // const {isInCart, setInCart} = useState(false);
+
+    // useEffect(() => {
+    //   const productIsInCart = cartList.find(cartItem => cartItem.id === id)
+      
+    //   if(productIsInCart){
+    //     setInCart(true);
+    //   }else{
+    //     setInCart(false);
+    //   }
+    
+    // }, [setInCart, cartList, id])
 
   return (
     <div className="productCard" >
@@ -9,7 +25,7 @@ export const ProductCard = ({product, handleClick}) => {
         <p className="name">{name}</p>
         <div className="action">
             <p> Ksh.{price} only!!</p>
-            <button onClick= {handleClick}>Add to Cart</button>
+            <button onClick= {() => addToCart(product)}>Add to Cart</button>
         </div>
     </div>
   )
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,18 +1,16 @@
 import {CartCard} from '../components'
 import { useTitle } from '../hooks/useTitle'
+import { useCart } from '../context/CartContext'
 
 export const Cart = () => {
+  const {total, cartList} = useCart();
   useTitle("Cart")
-  const products = [
-    {"id": 1, "name": "Sony Wh-Ch510 Bluetooth Wireless", "price": 14900, "image": "/assets/images/image1.jpg"},
-    {"id": 2, "name": "boAt Rockerz 450", "price": 4900, "image": "/assets/images/image2.jpg"}
-  ]
-
+  
   return (
     <main>
       <section className='cart'>
-      <h1>Cart Items: {products.length}</h1>
-      {products.map((product) => (
+      <h1>Cart Items: {cartList.length}/ Ksh. {total}</h1>
+      {cartList.map((product) => (
         <CartCard key={product.id} product= {product}/>
       ))}
 
