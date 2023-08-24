67bf52da788e297c13275fec1e42479eb485cbbf
Keely001 <thindiuke@gmail.com>
2023-08-24 22:36:00 +0300

    debugged

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,23 +1,23 @@
 import "./ProductCard.css"
 import {useCart} from "../context/CartContext"
-// import { useState } from "react";
-// import { useEffect } from "react";
+import { useState } from "react";
+import { useEffect } from "react";
 
 export const ProductCard = ({product}) => {
-    const {  name, price, image} = product;
-    const {addToCart} = useCart();
-    // const {isInCart, setInCart} = useState(false);
+    const { id, name, price, image} = product;
+    const {addToCart,cartList, removeFromCart} = useCart();
+    const [isInCart, setInCart] = useState(false);
 
-    // useEffect(() => {
-    //   const productIsInCart = cartList.find(cartItem => cartItem.id === id)
+    useEffect(() => {
+      const productIsInCart = cartList.find(cartItem => cartItem.id === id)
       
-    //   if(productIsInCart){
-    //     setInCart(true);
-    //   }else{
-    //     setInCart(false);
-    //   }
+      if(productIsInCart){
+        setInCart(true);
+      }else{
+        setInCart(false);
+      }
     
-    // }, [setInCart, cartList, id])
+    }, [cartList,id, setInCart])
 
   return (
     <div className="productCard" >
@@ -25,7 +25,7 @@ export const ProductCard = ({product}) => {
         <p className="name">{name}</p>
         <div className="action">
             <p> Ksh.{price} only!!</p>
-            <button onClick= {() => addToCart(product)}>Add to Cart</button>
+            {isInCart?  <button className="remove" onClick= {() => removeFromCart(product)}>Remove</button> : <button onClick= {() => addToCart(product)}>Add to Cart</button>}
         </div>
     </div>
   )
